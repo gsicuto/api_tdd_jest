8d646ad70d3883911a543182cfa29d3550c4ca09
Gabriel Sicuto <gabrielsicuto@gmail.com>
2022-03-22 19:09:14 -0300

    test: ensure sum function parse string correctly

--- src/firstTest/sum.spec.js
+++ src/firstTest/sum.spec.js
@@ -4,4 +4,9 @@ describe('First Test', () => {
   test('ensure 1 = 1', () => {
     expect(sum(1, 1)).toBe(2)
   })
+  test('Ensure sum fuction parse string values', () => {
+    expect(sum('1', 1)).toBe(2)
+    expect(sum(1, '1')).toBe(2)
+    expect(sum('1', '1')).toBe(2)
+  })
 })
