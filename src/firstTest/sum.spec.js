1ef10c5c25f30670134ac52b5a620e622d4235bc
Gabriel Sicuto <gabrielsicuto@gmail.com>
2022-03-22 22:50:01 -0300

    test: ensure return an error if is not possible to make the sum

--- src/firstTest/sum.spec.js
+++ src/firstTest/sum.spec.js
@@ -9,4 +9,7 @@ describe('First Test', () => {
     expect(sum(1, '1')).toBe(2)
     expect(sum('1', '1')).toBe(2)
   })
+  test('return an error if is not possible to make the sum', () => {
+    expect(() => sum(' ', 1)).toThrow(new Error('invalid'))
+  })
 })
